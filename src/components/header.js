8ec37fc8a10cbd4ebf64614470f6f4eaab739474
Nick Hart <nhartmusic@gmail.com>
2021-05-29 17:53:19 +0200

    styling some things

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,96 @@
-import * as React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import { Link, navigate } from 'gatsby'
 import { FirebaseContext } from './Firebase'
+import styled from 'styled-components'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to='/'
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <FirebaseContext.Consumer>
-        {props => {
-          console.log(props)
-          return <div></div>
-        }}
-      </FirebaseContext.Consumer>
-    </div>
-  </header>
-)
+const LogoutLink = styled.span`
+  color: white;
+  cursor: pointer;
 
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const HeaderWrapper = styled.header`
+  background: pink;
+  margin-bottom: 1.45rem;
+`
+
+const HeaderContent = styled.div`
+   margin: 0 auto;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+  display: flex;
+
+  >h1 {
+    margin: 0;
+    flex-grow: 1;
+
+    >a {
+      color: white;
+      text-decoration: none;
+    }
+  }
+
+  >div{
+    margin: auto 0;
+  }
+
+`
+
+const UserInfo = styled.div`
+  text-align: right;
+`
+
+const LoginLink = styled.div`
+  margin: auto 0;
+
+  a {
+    color: white;
+  }
+`
+
+const Header = ({ siteTitle }) => {
+  const {firebase, user} = useContext(FirebaseContext)
+  console.log(firebase, user)
+
+  function handleLogoutClick() {
+    firebase.logout().then(() => navigate('/login'))
+  }
+
+  return (
+    <HeaderWrapper>
+      <HeaderContent>
+        <h1>
+          <Link
+            to='/'
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <UserInfo>
+          {!!user && !!user.email &&
+            <div>
+              Hello, {user.email}
+              <br></br>
+              <LogoutLink onClick={handleLogoutClick}>
+                Logout
+              </LogoutLink>
+            </div>
+          }
+          {(!user || !user.email) && 
+            <LoginLink>
+              <Link to='/login'>Login</Link>
+            </LoginLink>
+          }
+        </UserInfo>
+      </HeaderContent>
+    </HeaderWrapper>
+  )
+}
+  
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }
